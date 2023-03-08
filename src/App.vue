b5319d973e5f7b8c695fbc5693be7019a86f0d29
cristianorneto@gmail.com <cristianorneto@gmail.com>
2023-03-08 17:22:18 -0300

    feat: build a app screenshot

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <header>
+      <h1>Take a screenshot</h1>
+    </header>
+    <main>
+      <form @submit.prevent="requestScreenshot">
+        <input type="url" name="url" id="url" v-model="url" />
+        <input type="submit" value="Take screenshot" />
+      </form>
+      <a
+        v-if="screenshotUrl != ''"
+        :href="screenshotUrl"
+        target="_blank"
+        download
+      >
+        <img :src="screenshotUrl" />
+      </a>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  setup() {
+    const url = ref("http://");
+    const screenshotUrl = ref("");
+
+    const requestScreenshot = async () => {
+      const res = await fetch("http://localhost:5000/screenshot", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          url: url.value,
+        }),
+      }).then((data) => data.json());
+      console.log(res);
+      screenshotUrl.value =
+        "http://localhost:5000/static/screenshots/" + res.ID + ".png";
+    };
+
+    return {
+      url,
+      screenshotUrl,
+      requestScreenshot,
+    };
+  },
+};
 </script>
 
 <style>
@@ -22,5 +61,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img {
+  display: block;
+  width: 400px;
+  max-width: 100%;
+  margin: 32px auto;
 }
 </style>
